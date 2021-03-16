3441525721bcb0df224d36aadf6f73af6a92e7e5
bejoy <73289790+BEJOY474@users.noreply.github.com>
2021-03-16 17:48:47 +0600

    restaurant page

---
+++ rastaurant.js
@@ -0,0 +1,7 @@
+// document.getElementById("icn-manu").addEventListener("click", function(){
+//     document.getElementById("nav").style.display="none";
+// });
+
+ document.getElementById("icn-manu").addEventListener("click", function(){
+     document.getElementById("nav").style.display="block";
+ });
